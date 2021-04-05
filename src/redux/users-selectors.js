e805c2f8037ce41685e954af866a13ffc1afc148
CarnageXD <bodya21111@gmail.com>
2021-04-05 11:44:14 +0300

    implemented reselest for complexity state

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,6 +1,12 @@
-export const getUsers = (state) => {
-    return state.friendsPage.users;
+import { createSelector } from "reselect"
+
+export const getUsersSelector = (state) => {
+    return state.friendsPage.users
 }
+
+export const getUsers = createSelector(getUsersSelector, (users) => {
+    return users.filter(u => true)
+})
 
 export const getPageSize = (state) => {
     return state.friendsPage.pageSize
